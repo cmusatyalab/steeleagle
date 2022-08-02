e26b51db16bfc1c5b75bc738b1e5bd21d1ec3d56
Thomas Eiszler <teiszler@andrew.cmu.edu>
2022-08-02 10:56:45 -0400

    Removing documentation for monocular-vision-based-obstacle-detection

--- survival/monocular-vision-based-obstacle-detection/docs/html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['batchnorm_151',['BatchNorm',['../classmrcnn_1_1model_1_1_batch_norm.html',1,'mrcnn::model']]],
-  ['box_152',['Box',['../classdata__model_1_1_box_1_1_box.html',1,'data_model::Box']]]
-];
--- survival/monocular-vision-based-obstacle-detection/docs/html/search/classes_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['kalmantracker_163',['KalmanTracker',['../classmatcher_1_1_kalman_tracker_1_1_kalman_tracker.html',1,'matcher::KalmanTracker']]],
-  ['kittievaluator_164',['KittiEvaluator',['../classutils_1_1_kitti_evaluator_1_1_kitti_evaluator.html',1,'utils::KittiEvaluator']]]
-];
