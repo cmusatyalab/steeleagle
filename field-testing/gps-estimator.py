Fixed yaw calculation and set yaw to degrees

field-testing/gps-estimator.py
@@ -14,14 +14,14 @@ def find_intersection(target_dir, target_insct):
 if __name__ == "__main__":
     # Establish parameters.
     EARTH_RADIUS = 6378137.0
-    drone_lat = 40.413173
-    drone_lon = -79.949509
+    drone_lat = 40.44271299824439
+    drone_lon = -79.93926999231915
     north_vec = np.array([0, 1, 0]) # Create a vector pointing due north.
-    camera_pitch = -25 # Pitch of the gimbal.
-    camera_yaw = 90 # Yaw of the drone.
-    camera_elev = 5 # Elevation of the drone.
-    target_x_pix = 380 # Location of target in X pixels.
-    target_y_pix = 120 # Location of target in Y pixels.
+    camera_pitch = -20.000099182128906 # Pitch of the gimbal.
+    camera_yaw = 2.320742130279541 * (180/np.pi) # Yaw of the drone.
+    camera_elev = 2.731494188308716 # Elevation of the drone.
+    target_x_pix = 80 # Location of target in X pixels.
+    target_y_pix = 293 # Location of target in Y pixels.
     HFOV = 69 # Horizontal FOV.
     VFOV = 43 # Vertical FOV.
     pixel_center = (640/2, 480/2) # Center pixel location of the camera.
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     print("Camera centered at: ({0}, {1}, {2})".format(camera_center[0], camera_center[1], camera_center[2]))
 
-    target_yaw_angle = -1 * ((target_x_pix - pixel_center[0]) / pixel_center[0]) * (HFOV / 2)
+    target_yaw_angle = ((target_x_pix - pixel_center[0]) / pixel_center[0]) * (HFOV / 2)
     target_pitch_angle = ((target_y_pix - pixel_center[1]) / pixel_center[1]) * (VFOV / 2)
     r = R.from_euler('ZYX', [camera_yaw + target_yaw_angle, 0, camera_pitch + target_pitch_angle], degrees=True) # Rotate the camera center vector to target center.
     target_dir = r.as_matrix().dot(north_vec)