744f3b6c1de41e6efdde7a2e677983da4be1fd3b
Thomas Eiszler <teiszler@andrew.cmu.edu>
2023-11-30 09:28:06 -0500

    Removing hardcoded refs

--- hermes/java/utils/set-env.sh
+++ hermes/java/utils/set-env.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
-export JAVAC_PATH="/home/mbala/android-studio/jbr/bin/javac" 
-export D8_PATH="/home/mbala/Android/Sdk/build-tools/33.0.2/d8"
-export ANDROID_SDK_PATH="/home/mbala/Android/Sdk/platforms/android-33/android.jar"
+export JAVAC_PATH="/path/to/android-studio/jbr/bin/javac" 
+export D8_PATH="/path/to/Android/Sdk/build-tools/33.0.2/d8"
+export ANDROID_SDK_PATH="/path/to/Android/Sdk/platforms/android-33/android.jar"
