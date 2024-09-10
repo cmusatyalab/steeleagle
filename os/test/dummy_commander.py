b1d9aa81db392aceb9a391691ae18e12f6041b36
Mihir Bala <mkbala42@gmail.com>
2024-09-10 12:42:21 -0400

    Added manual control to dummy commander

--- os/test/dummy_commander.py
+++ os/test/dummy_commander.py
@@ -4,7 +4,7 @@ import time
 from cnc_protocol import cnc_pb2
 import asyncio
 import zmq.asyncio
-
+import keyboard
 
 context = zmq.Context()
 
@@ -24,6 +24,29 @@ class c_client():
         cmd_front_socket.send_multipart([message])
         print(f"commander: take off sent at: {time.time()}")
         
+    def send_land(self):
+        driver_command = cnc_pb2.Extras()
+        driver_command.cmd.land = True
+        message = driver_command.SerializeToString()
+        cmd_front_socket.send_multipart([message])
+        print(f"commander: land sent at: {time.time()}")
+   
+    def send_MCOM(self, key):
+        driver_command = cnc_pb2.Extras()
+        driver_command.cmd.setVelocity = cnc_pb2.setVelocity()
+        match key:
+            case 'w':
+                driver_command.cmd.setVelocity.forward_vel = 2.0
+            case 's':
+                driver_command.cmd.setVelocity.forward_vel = -2.0
+            case 'a':
+                driver_command.cmd.setVelocity.right_vel = 2.0
+            case 'd':
+                driver_command.cmd.setVelocity.right_vel = -2.0
+        message = driver_command.SerializeToString()
+        cmd_front_socket.send_multipart([message])
+        print(f"commander: manual command of \'{key}\' sent at: {time.time()}")
+
     def send_startM(self):
         driver_command = cnc_pb2.Extras()
         driver_command.cmd.script_url = "https://www.ant.com"
@@ -33,15 +56,19 @@ class c_client():
         
     async def a_run(self):
         # Interactive command input loop
-        
+        MCOM_SET = ['w', 'a', 's', 'd', 'i', 'j', 'k', 'l']
         while True:
-            user_input = input("Enter 'takeoff' to send the take off cmd: ").strip().lower()
+            user_input = keyboard.read_key()
  
-            if user_input == 'takeoff':
+            if user_input == 't':
                 self.send_takeOff()
-            elif user_input == 'start':
+            elif user_input == 'g':
+                self.send_land()
+            elif user_input == 'm':
                 self.send_startM()
-            elif user_input == "exit":
+            elif user_input in MCOM_SET:
+                self.send_MCOM(user_input)
+            elif user_input == "x":
                 print("Exiting client.")
                 break
             else:
