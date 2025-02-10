56f89d76a80560db46afd003150e09f33f53a839
Aditya Chanana <achanana@cs.cmu.edu>
2025-02-10 15:14:25 -0500

    Fix log levels for data store

--- os/kernel/DataStore.py
+++ os/kernel/DataStore.py
@@ -19,10 +19,10 @@ class DataStore:
     ######################################################## COMPUTE ############################################################
     def get_compute_result(self, compute_id, compute_type: str) -> Optional[Union[None, tuple]]:
         pass
-    
+
     def append_compute(self, compute_id):
         self._result_cache[compute_id] = {}
-        
+
     def update_compute_result(self, compute_id, compute_type: str, result, timestamp):
         assert isinstance(compute_type, str), f"Argument must be a string, got {type(compute_type).__name__}"
         self._result_cache[compute_id][compute_type] = (result, timestamp)
@@ -33,26 +33,26 @@ class DataStore:
         data_copy_type = type(data_copy)
         if data_copy_type not in self._raw_data_cache:
             # Log an error and return None
-            logger.debug(f"get_raw_data: No such data: data type {data_copy_type}")
+            logger.error(f"get_raw_data: No such data: data type {data_copy_type}")
             return None
-        
+
         cache = self._raw_data_cache.get(data_copy_type)
         if cache is None:
             # Log an error and return None
-            logger.debug(f"get_raw_data: No data found for data type {data_copy_type}")
+            logger.error(f"get_raw_data: No data found for data type {data_copy_type}")
             return None
-        
+
         # Create a copy of the protobuf message
         data_copy.CopyFrom(cache)
-        
+
         # # Clear the cache
         # self._raw_data_cache[data_copy_type] = None
-    
+
 
     def set_raw_data(self, data):
         data_type = type(data)
         if data_type not in self._raw_data_cache:
-            logger.debug(f"set_raw_data: No such data: data type {data_type}")
+            logger.error(f"set_raw_data: No such data: data type {data_type}")
             return None
 
         self._raw_data_cache[data_type] = data
