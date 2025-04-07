18a9b404be1bc744ec64092a213dd8c7a350b62e
Mihir Bala <mkbala42@gmail.com>
2025-04-07 15:27:06 -0400

    Fixes to test GPS but failing for PX4

--- vehicle/drivers/multicopter/test/auto_test_suite.py
+++ vehicle/drivers/multicopter/test/auto_test_suite.py
@@ -198,20 +198,20 @@ class TestSuiteClass:
         ]
 
         for (dy, dx, dz, d_angle) in test_sets:
-            d_lat = self.dy_to_lat(self.d_lat)
-            d_lon = self.dx_to_lon(self.d_lon, curr_pos_lat)
+            d_lat = self.dy_to_lat(dy)
+            d_lon = self.dx_to_lon(dx, curr_pos_lat)
 
             # Build the *new* absolute position from the current telemetry
             next_lat = curr_pos_lat + d_lat
             next_lon = curr_pos_lon + d_lon
-            next_alt = curr_pos_alt + self.d_alt # Relative altitude
-            next_angle = curr_pos_angle + self.d_angle
+            next_alt = curr_pos_alt + dz # Relative altitude
+            next_angle = curr_pos_angle + d_angle
 
             logger.info(f"Testing set GPS position to: {(next_lat, next_lon, next_alt, next_angle)}")
             driver_cmd = control_protocol.Request()
             driver_cmd.veh.location.latitude  = next_lat
             driver_cmd.veh.location.longitude = next_lon
-            driver_cmd.veh.location.altitude  = self.d_alt
+            driver_cmd.veh.location.altitude  = next_alt
             driver_cmd.veh.location.bearing   = next_angle
             seq = sequence_counter["value"]
             logger.info(f"Sending command with seqNum: {seq}")
