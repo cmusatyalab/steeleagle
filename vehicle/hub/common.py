327f8c15d855d1f5bf599f9b219755f520177554
Aditya Chanana <achanana@cs.cmu.edu>
2025-04-08 14:49:53 -0400

    Revert "Use config yaml file for hub service"
    This reverts commit 741de7fb423d0d7c6ee62b1be6b185c272fd92d8.

---
+++ vehicle/hub/common.py
@@ -0,0 +1,1 @@
+logging_format = "%(asctime)s [%(levelname)s] (%(filename)s:%(lineno)d) - %(message)s"
--- vehicle/util/utils.py
+++ vehicle/util/utils.py
@@ -1,7 +1,6 @@
 from enum import Enum
 import logging
 import os
-import yaml
 import zmq
 import zmq.asyncio
 
@@ -66,9 +65,4 @@ async def lazy_pirate_request(socket, payload, ctx, server_endpoint, retries=3,
 
         logger.info(f"Resending payload to {server_endpoint=}...")
         socket.send(payload)
-
-def import_config(config_path):
-    with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-        return config
 
