cca59e65d37cf7180dd261f998a94d528aa9ddde
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-05-06 08:37:51 -0400

    Send the results back to the drone if it is a new object OR if it is an existing object that was found by this drone (so it can continue to track it).

--- backend/server/engines/detection/openscout_object_engine.py
+++ backend/server/engines/detection/openscout_object_engine.py
@@ -146,28 +146,18 @@ class OpenScoutObjectEngine(cognitive_engine.Engine):
 
     def storeDetection(self, drone, lat, lon, cls, conf, link=""):
         object_name = f"{cls}-{os.urandom(2).hex()}"
-        # first do a geosearch to see if there is a match within radius
-        objects = self.r.geosearch(
-            "detections",
-            longitude=np.clip(lon, -180, 180),
-            latitude=np.clip(lat, -90, 90),
-            radius=self.search_radius,
-            unit="m",
-        )
+        self.r.geoadd("detections", [lon, lat, object_name])
 
-        if len(objects) == 0:
-            self.r.geoadd("detections", [lon, lat, object_name])
-
-            object_key = f"objects:{object_name}"
-            self.r.hset(object_key, "last_seen", f"{time.time()}")
-            self.r.hset(object_key, "drone_id", f"{drone}")
-            self.r.hset(object_key, "cls", f"{cls}")
-            self.r.hset(object_key, "confidence", f"{conf}")
-            self.r.hset(object_key, "link", f"{link}")
-            self.r.hset(object_key, "longitude", f"{lon}")
-            self.r.hset(object_key, "latitude", f"{lat}")
-            self.r.expire(object_key, self.ttl_secs)
-            logger.debug(f"Updating {object_key} status: last_seen: {time.time()}")
+        object_key = f"objects:{object_name}"
+        self.r.hset(object_key, "last_seen", f"{time.time()}")
+        self.r.hset(object_key, "drone_id", f"{drone}")
+        self.r.hset(object_key, "cls", f"{cls}")
+        self.r.hset(object_key, "confidence", f"{conf}")
+        self.r.hset(object_key, "link", f"{link}")
+        self.r.hset(object_key, "longitude", f"{lon}")
+        self.r.hset(object_key, "latitude", f"{lat}")
+        self.r.expire(object_key, self.ttl_secs)
+        logger.debug(f"Updating {object_key} status: last_seen: {time.time()}")
 
     def passes_hsv_filter(self, image, bbox, hsv_min=[30,100,100], hsv_max=[50,255,255], threshold=5.0,) -> bool:
         cropped = image[round(bbox[0]):round(bbox[2]), round(bbox[1]):round(bbox[3])]
@@ -281,16 +271,38 @@ class OpenScoutObjectEngine(cognitive_engine.Engine):
                     lower_bound = [cpt_config.lower_bound.H, cpt_config.lower_bound.S, cpt_config.lower_bound.V]
                     upper_bound = [cpt_config.upper_bound.H, cpt_config.upper_bound.S, cpt_config.upper_bound.V]
                     hsv_filter = self.passes_hsv_filter(image_np, box, lower_bound, upper_bound, threshold=self.hsv_threshold)
+                # first do a geosearch to see if there is a match within radius
+                objects = self.r.geosearch(
+                    "detections",
+                    longitude=np.clip(lon, -180, 180),
+                    latitude=np.clip(lat, -90, 90),
+                    radius=self.search_radius,
+                    unit="m",
+                )
 
-                r.append({
-                    "id": i,
-                    "class": names[i],
-                    "score": scores[i],
-                    "lat": lat, "lon":
-                    lon, "box": box,
-                    "hsv_filter": hsv_filter
-                })
-                self.storeDetection(drone_id, lat, lon, names[i],scores[i], os.environ["WEBSERVER"]+"/detected/"+filename if self.store_detections else "" )
+                if len(objects) == 0:
+                    r.append({
+                        "id": i,
+                        "class": names[i],
+                        "score": scores[i],
+                        "lat": lat, "lon":
+                        lon, "box": box,
+                        "hsv_filter": hsv_filter
+                    })
+                    self.storeDetection(drone_id, lat, lon, names[i],scores[i], os.environ["WEBSERVER"]+"/detected/"+filename if self.store_detections else "" )
+                else:
+                    for obj in objects:
+                        id = self.r.hget(obj, "drone_id")
+                        if id == drone_id:
+                            r.append({
+                                "id": i,
+                                "class": names[i],
+                                "score": scores[i],
+                                "lat": lat, "lon":
+                                lon, "box": box,
+                                "hsv_filter": hsv_filter
+                            })
+                            self.storeDetection(drone_id, lat, lon, names[i],scores[i], os.environ["WEBSERVER"]+"/detected/"+filename if self.store_detections else "" )
 
         if not detections_above_threshold:
             return None
