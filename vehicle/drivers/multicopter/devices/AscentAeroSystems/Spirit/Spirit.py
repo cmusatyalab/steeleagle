c6d3d2f7acfbc21e52e090b4ac4c107b5ba13c3b
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-09-10 11:00:20 -0400

    Override the mode mappings because the ones we get back from the Spirit are for fixed wing instead of multicopter.

--- vehicle/drivers/multicopter/devices/AscentAeroSystems/Spirit/Spirit.py
+++ vehicle/drivers/multicopter/devices/AscentAeroSystems/Spirit/Spirit.py
@@ -12,6 +12,37 @@ logger = logging.getLogger(__name__)
 
 
 class Spirit(ArduPilotDrone):
+    # default multicopter mappings
+    mode_mapping_acm = {
+        "STABILIZE": 0,
+        "ACRO": 1,
+        "ALT_HOLD": 2,
+        "AUTO": 3,
+        "GUIDED": 4,
+        "LOITER": 5,
+        "RTL": 6,
+        "CIRCLE": 7,
+        "POSITION": 8,
+        "LAND": 9,
+        "OF_LOITER": 10,
+        "DRIFT": 11,
+        "SPORT": 13,
+        "FLIP": 14,
+        "AUTOTUNE": 15,
+        "POSHOLD": 16,
+        "BRAKE": 17,
+        "THROW": 18,
+        "AVOID_ADSB": 19,
+        "GUIDED_NOGPS": 20,
+        "SMART_RTL": 21,
+        "FLOWHOLD": 22,
+        "FOLLOW": 23,
+        "ZIGZAG": 24,
+        "SYSTEMID": 25,
+        "AUTOROTATE": 26,
+        "AUTO_RTL": 27,
+    }
+
     def __init__(self, drone_id, **drone_args):
         super().__init__(drone_id)
 
@@ -22,3 +53,9 @@ class Spirit(ArduPilotDrone):
 
     async def set_gimbal_pose(self, pose):
         return common_protocol.ResponseStatus.NOTSUPPORTED
+
+    async def connect(self, connection_string):
+        super().connect(connection_string)
+        # override the mode mapping because the mav_type is not reported properly
+        # and we end up getting the mappings for a fixed wing
+        self._mode_mapping = self.mode_mapping_acm
