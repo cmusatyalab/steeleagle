061a275979a5d8e8e498574a181e78d6951159b2
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-09-29 12:51:22 -0400

    WIP on drone cards.

--- gcs/hawkeye/src/DroneCard.jsx
+++ gcs/hawkeye/src/DroneCard.jsx
@@ -1,40 +1,62 @@
 import { useState, useRef, useEffect } from 'react';
 import './DroneCard.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Stack from 'react-bootstrap/Stack'
 import Button from 'react-bootstrap/Button';
-function DroneCard({ name, index, data }) {  
+
+
+function DroneCard({ data, onClick }) {
+    var battery_icon = ""
+    var battery_style = "success"
+    if (data.battery > 75) {
+        battery_icon ="fa-battery-full"
+        battery_style="success"
+    }
+    else if (data.battery <= 75 && data.battery >50) {
+        battery_icon ="fa-battery-three-quarters"
+        battery_style="success"
+    }
+    else if (data.battery <= 50 && data.battery >25) {
+        battery_icon ="fa-battery-half"
+        battery_style="warning"
+    }
+    else if (data.battery <= 25 && data.battery >0) {
+        battery_icon ="fa-battery-quarter"
+        battery_style="danger"
+    }
+    else {
+        battery_icon ="fa-battery-empty"
+        battery_style="danger"
+    }
 
 
   return (
-            <Button variant="dark">
-            <Card key={index} bg='dark' text='light' border='light' >
+            <Button variant="dark" onClick={onClick}>
+            <Card  bg='dark' text='light' border='light' >
             <Card.Body>
-                <Card.Title >🚁{name}</Card.Title>
-                <Card.Subtitle>Parrot ANAFI</Card.Subtitle>
-                
+                <Card.Title >🚁{data.name}</Card.Title>
+                <Card.Subtitle>{data.model}</Card.Subtitle>
                     <Stack direction="horizontal" gap={1} style={{ margin: '5px', }}>
-                    <Badge pill bg="success">
-                        Battery: 97%
+                    <Badge pill bg={battery_style}>
+                        <FontAwesomeIcon icon={`fas-solid ${battery_icon}`} /> {data.battery}%
                     </Badge>
-                    <Badge pill bg="warning">
-                        Satellites: 12
+                    <Badge pill bg="secondary">
+                        <FontAwesomeIcon icon="fas-solid fa-satellite" /> {data.sats}
+                    </Badge>
+                    <Badge pill bg="primary">
+                        Task: {data.task}
                     </Badge>
                     </Stack>
                     <Stack direction="horizontal" gap={1} style={{ margin: '5px', }}>
-                    <Badge pill bg="danger">
-                        Task: idle
-                    </Badge>
-                    <Badge pill bg="info">
-                        Something else
-                    </Badge>
-                  
+
                     </Stack>
             </Card.Body>
             <Card.Footer>
-                Last updated 3 mins ago
+                Last updated {data.last_updated} seconds ago
             </Card.Footer>
             </Card>
             </Button>
