0dc5d37600eb18064128315e952d015d7f678ae2
Mihir Bala <mkbala42@gmail.com>
2025-10-07 23:20:28 -0400

    Added skeleton for docs generation

---
+++ sdk/build/generate/generate_docs.py
@@ -0,0 +1,19 @@
+from docstring_parser import parse
+import griffe
+
+_API_MODULE_PATH = '../../api'
+_DSL_MODULE_PATH = '../../dsl'
+
+def generate_pyfile_docs():
+    # API Module Docs
+    api = griffe.load(_API_MODULE_PATH) 
+    for name, member in api.modules.items():
+        print(name, member)
+        if member.is_module:
+            for n, m in member.modules.items():
+                print(n)
+
+def generate_protobuf_docs():
+    pass
+
+generate_pyfile_docs()
