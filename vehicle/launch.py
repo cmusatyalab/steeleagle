745e8f2f6cf722e2cc0174c89d63d626577b9d0f
Mihir Bala <mkbala42@gmail.com>
2025-11-06 17:03:08 -0500

    Added headless mode option to launch without driver

--- vehicle/launch.py
+++ vehicle/launch.py
@@ -17,20 +17,21 @@ def start_services(log, info):
         task = subprocess.Popen(logger)
         running.append(task)
     # Start the driver
-    if info["kwargs"]:
-        driver = ['uvx', 
-                  '--extra-index-url', ROOST,
-                  '--python', info["python"],
-                  info["package"], '--kwargs', json.dumps(info["kwargs"]), info["name"], info["address"], info["telemetry"], info["imagery"]]
-        print(driver)
-    else:
-        driver = ['uvx', 
-                  '--extra-index-url', ROOST,
-                  '--python', info["python"],
-                  info["package"], info["name"], info["address"], info["telemetry"], info["imagery"]]
-    task = subprocess.Popen(driver)
-    running.append(task)
-    time.sleep(5)
+    if info:
+        if info["kwargs"]:
+            driver = ['uvx', 
+                      '--extra-index-url', ROOST,
+                      '--python', info["python"],
+                      info["package"], '--kwargs', json.dumps(info["kwargs"]), info["name"], info["address"], info["telemetry"], info["imagery"]]
+            print(driver)
+        else:
+            driver = ['uvx', 
+                      '--extra-index-url', ROOST,
+                      '--python', info["python"],
+                      info["package"], info["name"], info["address"], info["telemetry"], info["imagery"]]
+        task = subprocess.Popen(driver)
+        running.append(task)
+        time.sleep(5)
     # Start core services
     core = [
         ['python', 'mission/main.py'], # Mission
@@ -70,6 +71,7 @@ if __name__ == '__main__':
     parser.add_argument('--config', type=str, default='config.toml', help='override config file path (default: config.toml)')
     parser.add_argument('--internal', type=str, default='.internal.toml', help='override internal config file path (default: .internal.toml)')
     parser.add_argument('--law', type=str, default='.laws.toml', help='override law config file path (default: .laws.toml)')
+    parser.add_argument('--headless', action='store_true', default=False, help='starts the kernel without a driver (default: False)')
     args = parser.parse_args()
 
     # Set environment variables here
@@ -80,16 +82,19 @@ if __name__ == '__main__':
     
     from util.config import query_config
     log = query_config('logging.generate_flight_log')
-    kwargs = query_config('vehicle.kwargs')
-    driver_info = {
-            'name': query_config('vehicle.name'),
-            'package': query_config('vehicle.package'),
-            'python': query_config('vehicle.python'),
-            'address': query_config('internal.services.driver'),
-            'telemetry': query_config('internal.streams.driver_telemetry'),
-            'imagery': query_config('internal.streams.imagery'),
-            'kwargs': kwargs if kwargs != {} else None
-    }
+    
+    driver_info = None
+    if not args.headless:
+        kwargs = query_config('vehicle.kwargs')
+        driver_info = {
+                'name': query_config('vehicle.name'),
+                'package': query_config('vehicle.package'),
+                'python': query_config('vehicle.python'),
+                'address': query_config('internal.services.driver'),
+                'telemetry': query_config('internal.streams.driver_telemetry'),
+                'imagery': query_config('internal.streams.imagery'),
+                'kwargs': kwargs if kwargs != {} else None
+        }
 
     if args.test:
         test_services(args.test, log)
