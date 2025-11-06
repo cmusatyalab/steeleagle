c3304a5650dce3e3460dd399797dd45e1d07abb1
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-11-06 13:09:22 -0500

    Fix color on overview page to match pages/control

--- gcs/streamlit/overview.py
+++ gcs/streamlit/overview.py
@@ -74,13 +74,13 @@ def draw_map():
                         text = folium.DivIcon(
                             icon_size="null",  # set the size to null so that it expands to the length of the string inside in the div
                             icon_anchor=(-20, 30),
-                            html=f'<div style="color:white;font-size: 12pt;font-weight: bold;background-color:{ColorHash({drone_name}).hex};">{drone_name}</div>',
+                            html=f'<div style="color:white;font-size: 12pt;font-weight: bold;background-color:{ColorHash(drone_name).hex};">{drone_name}</div>',
                             # TODO: concatenate current task to html once it is sent i.e. <i>PatrolTask</i></div>
                         )
                         plane = folium.Icon(
                             icon="plane",
                             color="lightgray",
-                            icon_color=ColorHash({drone_name}).hex,
+                            icon_color=ColorHash(drone_name).hex,
                             prefix="glyphicon",
                             angle=int(row["bearing"]),
                         )
