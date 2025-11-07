d656b45b43e50ca18e5c7104f65ce4dc6fc1dd27
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-11-07 11:08:54 -0500

    Only warn about broken links.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
   deploymentBranch: 'main',
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
