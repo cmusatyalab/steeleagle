e933543277d72cf277054be38953a1f6e4ca108d
Thomas Eiszler <teiszler@andrew.cmu.edu>
2025-11-26 10:45:56 -0500

    Removing docusaurus homepage features and adding our own.

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -1,45 +1,70 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { Icon } from "@iconify/react";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Accessible',
+    icon: 'healthicons:entry',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        SteelEagle lowers the barrier to entry for autonomous robotics by abstracting away disparate control schemes and providing a high level domain-specific language for writing missions.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Lightweight',
+    icon: 'noto:baby-chick',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Leveraging the edge for compute, SteelEagle enables autonomy on aircraft significantly smaller and lighter than traditional autonomous UAVs.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Portable',
+    icon: 'noto:floppy-disk',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Agnostic by design, Steeleagle supports heterogenous vehicle swarms allowing users to focus on the mission and worry less about the idiosyncrasies of the platforms available for that mission.
+      </>
+    ),
+  },
+  {
+    title: 'BVLOS (Beyond Visual Line-of-Sight)',
+    icon: 'flat-color-icons:binoculars',
+    description: (
+      <>
+        SteelEagle vehicles can communicate with an edge backend using any type of underlying radio communication, including LTE, without human supervision.
+      </>
+    ),
+  },
+  {
+    title: 'Scalable',
+    icon: 'bi:gpu-card',
+    description: (
+      <>
+        Utilizing edge computing allows SteelEagle vehicles to run computational expensive AI without carrying everything on board. As new AI technology emerges or edge hardware improves, all vehicles benefit without upgrading or retrofitting them.
+      </>
+    ),
+  },
+  {
+    title: 'Extensible',
+    icon: 'dashicons:plugins-checked',
+    description: (
+      <>
+        Designed for extensibility, developers can plug in their own components (vehicle-specific drivers, custom GCS apps, alternative mission control schemes) as long they adhere to the API specification.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({icon, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Icon icon={icon} widht='72px' height='72px'/>
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
