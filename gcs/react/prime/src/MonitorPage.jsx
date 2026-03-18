38ae94c55aa18ff1ddf9ed12b8f58ed8fd4ea98c
Thomas Eiszler <teiszler@andrew.cmu.edu>
2026-03-18 09:50:59 -0400

    Basic layout with one large map and status components underneath.

--- gcs/react/prime/src/MonitorPage.jsx
+++ gcs/react/prime/src/MonitorPage.jsx
@@ -1,10 +1,25 @@
 import Status from './Status.jsx';
+import Mapbox from './Mapbox.jsx';
+import { Panel } from 'primereact/panel';
 
-function MonitorPage({vehicles}) {
-const list = vehicles.map((v) =>  <Status vehicle={v}/>);
+function MonitorPage({ vehicles }) {
+    const list = vehicles.map((v) => <Status vehicle={v} />);
 
-    return (<>{list}</>);
 
+    return (
+        <>
+            <div className="flex flex-column">
+                <Panel header="Monitor" className="h-full" >
+                    <div className="grid m-0">
+                        <div className="col-12 lg:col-6 p-2"></div>
+                        <Mapbox selectedVehicle={null} tracking={false} mapPanelSize={0} vehicles={vehicles} />
+                        {vehicles.map((v) => <div className="col-12 lg:col-3 p-2"> <Status vehicle={v} /> </div>)}
+                    </div>
+                </Panel >
+
+            </div >
+        </>
+    );
 }
 
 export default MonitorPage;
