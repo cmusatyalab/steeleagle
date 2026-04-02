c158816ef2b37cd4ca38b55a991f2af2f3281b48
Mihir Bala <mkbala42@gmail.com>
2026-04-02 13:07:38 -0400

    New protocol

--- runtime/protos/descriptor.go
+++
@@ -1,6 +0,0 @@
-package protos
-
-import _ "embed"
-
-//go:embed protos.desc
-var DescriptorFile []byte // Descriptor file containing protocol file descriptor set
--- runtime/tests/mock.go
+++ runtime/tests/mock.go
@@ -7,29 +7,24 @@ import (
 )
 
 type controlServer struct {
-    pb.UnimplementedControlServer
+    pb.UnimplementedControlServiceServer
 }
 
-func (i *controlServer) Arm(_ context.Context, in *pb.ArmRequest) (*pb.Response, error) {
-    return &pb.Response{Status: 2}, nil
-}
-
-func (i *controlServer) TakeOff(in *pb.TakeOffRequest, stream pb.Control_TakeOffServer) error {
-    if err := stream.Send(&pb.Response{Status: 1}); err != nil {
-        return err
-    }
-    stream.Send(&pb.Response{Status: 2})
+func (i *controlServer) TakeOff(in *pb.TakeOffRequest, stream pb.ControlService_TakeOffServer) error {
+    stream.Send(&pb.Response{})
     return nil
 }
 
 type missionServer struct {
-    pb.UnimplementedMissionServer
+    pb.UnimplementedMissionServiceServer
 }
 
-func (i *missionServer) Start(_ context.Context, in *pb.StartRequest) (*pb.Response, error) {
-    return &pb.Response{Status: 2}, nil
+func (i *missionServer) Start(in *pb.StartRequest, stream pb.MissionService_StartServer) error {
+    stream.Send(&pb.Response{})
+    return nil
 }
 
-func (i *missionServer) Stop(_ context.Context, in *pb.StopRequest) (*pb.Response, error) {
-    return &pb.Response{Status: 2}, nil
+func (i *missionServer) Stop(in *pb.StopRequest, stream pb.MissionService_StopServer) error {
+    stream.Send(&pb.Response{})
+    return nil
 }
