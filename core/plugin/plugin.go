a18b2cc681204fd7e63fa59dce1aad953d23b1a7
Aditya Chanana <achanana@cs.cmu.edu>
2026-05-11 12:07:00 -0400

    Changes to plugin code

--- core/plugin/plugin.go
+++ core/plugin/plugin.go
@@ -1,105 +1,143 @@
 package plugin
 
 import (
-    "os/exec"
-    "context"
-    "path/filepath"
+	"context"
+	"os/exec"
+	"path/filepath"
 
-    "github.com/adrg/xdg"
-    "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 )
 
 type PluginRuntime int
 
 const (
-    Process   PluginRuntime = iota
-    Container PluginRuntime
-    Sandbox   PluginRuntime
+	Process PluginRuntime = iota
+	Container
+	Sandbox
 )
 
-type Plugin struct {
-    name      string
-    path      string
-    runtime   PluginRuntime
-    // Runtime attributes
-    pid       int
-    cid       string // container ID, if running as a container
-    spath     string // socket path, if running as a server connection type
-    proc      *exec.Cmd // process object, if running as a process
-    start     float64 // process start time, used to differentiate PIDs if they are recycled
-    // Context related attributes
-    ctx       context.Context
-    cancel    context.CancelFunc
+var pluginRuntimeName = map[PluginRuntime]string{
+	Process:   "process",
+	Container: "container",
+	Sandbox:   "sandbox",
 }
 
-func CreatePlugin(parentCtx context.Context, options ...PluginOption) (*Plugin, error) {
-    // Set the path to the plugin directory
-    if i.path == "" {
-        i.path = filepath.Join(xdg.DataDir, ApplicationName, i.Name)
-    }
+func (pr PluginRuntime) String() string {
+	return pluginRuntimeName[pr]
 }
 
-func (i *Plugin) Start(map[string]string envVars) error {
-    switch i.Runtime {
-        case PluginRuntime.Process:
-            // Start the process
-            cmd := exec.Command(filepath.Join(i.Path, PluginRunScript))
-            err := cmd.Run()
-            if err != nil {
-                log.Error().Err(err).Str("plugin", i.Name).Msg("could not run process for plugin")
-                return err
-            }
-        case PluginRuntime.Container:
-            // Check if image exists locally
-            exists, err := images.Exists(ctx, i.name, nil)
-            if err != nil {
-                log.Error().Err(err).Str("plugin", i.name).Msg("could not check for container images")
-                return err
-            }
-            if !exists {
-                log.Warn().Str("plugin", i.name).Msg("could not find container locally, attempting to pull")
-            }
-            
-            // Proceed to create + start as normal
-            s := specgen.NewSpecGenerator(i.name, false)
+type Plugin interface {
+	Start() error
+	Stop()
+	IsRunning() bool
+	Path() string
+}
 
-            // Create mounts for the right files
-            
-            resp, err := containers.CreateWithSpec(ctx, s, nil)
-            if err != nil {
-                log.Error().Err(err).Str("plugin", i.name).Msg("could not create container, aborting")
-                return err
-            }
-            // Set the container ID
-            i.cid = resp.ID
+type BasePlugin struct {
+	name string
+	path string
+	// Runtime attributes
+	spath string  // socket path, if running as a server connection type
+	start float64 // plugin start time
+}
 
-            // TODO: look up PID from CID
-        case PluginRuntime.Sandbox:
-    }
+type ProcessPlugin struct {
+	BasePlugin
+	pid  int
+	proc *exec.Cmd
+}
+
+type ContainerPlugin struct {
+	BasePlugin
+	cid string
+}
+
+type PluginOption func(*BasePlugin)
+
+func CreateProcessPlugin(name, path, spath string) (*ProcessPlugin, error) {
+	return nil, nil
+}
+
+func CreateContainerPlugin(name, path, spath, image_tag string) (*ContainerPlugin, error) {
+	return nil, nil
+}
+
+//func CreatePlugin(options ...PluginOption) (*Plugin, error) {
+//	// Set the path to the plugin directory
+//	if i.path == "" {
+//		i.path = filepath.Join(xdg.DataDir, ApplicationName, i.Name)
+//	}
+//}
+
+func (p ProcessPlugin) Start(ctx context.Context, envVarsmap map[string]string) error {
+	// Start the process
+	cmd := exec.Command(filepath.Join(p.path, PluginRunScript))
+	err := cmd.Run()
+	if err != nil {
+		log.Error().Err(err).Str("plugin", i.Name).Msg("could not run process for plugin")
+		return err
+	}
+
+	//switch i.Runtime {
+	//case PluginRuntime.Process:
+	//	// Start the process
+	//	cmd := exec.Command(filepath.Join(i.Path, PluginRunScript))
+	//	err := cmd.Run()
+	//	if err != nil {
+	//		log.Error().Err(err).Str("plugin", i.Name).Msg("could not run process for plugin")
+	//		return err
+	//	}
+	//case PluginRuntime.Container:
+	//	// Check if image exists locally
+	//	exists, err := images.Exists(ctx, i.name, nil)
+	//	if err != nil {
+	//		log.Error().Err(err).Str("plugin", i.name).Msg("could not check for container images")
+	//		return err
+	//	}
+	//	if !exists {
+	//		log.Warn().Str("plugin", i.name).Msg("could not find container locally, attempting to pull")
+	//	}
+
+	//	// Proceed to create + start as normal
+	//	s := specgen.NewSpecGenerator(i.name, false)
+
+	//	// Create mounts for the right files
+
+	//	resp, err := containers.CreateWithSpec(ctx, s, nil)
+	//	if err != nil {
+	//		log.Error().Err(err).Str("plugin", i.name).Msg("could not create container, aborting")
+	//		return err
+	//	}
+	//	// Set the container ID
+	//	i.cid = resp.ID
+
+	//	// TODO: look up PID from CID
+	//case PluginRuntime.Sandbox:
+	//}
 
 }
 
 // Status methods
 func (i *Plugin) Name() string {
-    return i.name
+	return i.name
 }
 
 func (i *Plugin) Path() string {
-    return i.path
+	return i.path
 }
 
 func (i *Plugin) Runtime() PluginRuntime {
-    return i.runtime
+	return i.runtime
 }
 
 func (i *Plugin) ConnType() PluginConnType {
-    return i.connType
+	return i.connType
 }
 
 func (i *Plugin) PID() int {
-    return i.pid
+	return i.pid
 }
 
 func (i *Plugin) IsRunning() bool {
-    return i.pid != 0
+	return i.pid != 0
 }
--- core/vehicle/data_service.go
+++ core/vehicle/data_service.go
@@ -3,22 +3,25 @@ package vehicle
 import (
 	"context"
 
-	pb "github.com/cmusatyalab/steeleagle/api/gen/go/v1/services"
+	messages_pb "github.com/cmusatyalab/steeleagle/api/gen/go/v1/messages"
+	services_pb "github.com/cmusatyalab/steeleagle/api/gen/go/v1/services"
 )
 
 type DataService struct {
-	pb.UnimplementedDataServiceServer
-	vehicle *Vehicle
+	services_pb.UnimplementedDataServiceServer
+	vehicle          *Vehicle
+	latest_telemetry *messages_pb.Telemetry
+	latest_frame     *messages_pb.EncodedFrame
 }
 
-func (s *DataService) GetResult(ctx context.Context, req *pb.GetResultRequest) (*pb.GetResultResponse, error) {
+func (s *DataService) GetResult(ctx context.Context, req *services_pb.GetResultRequest) (*services_pb.GetResultResponse, error) {
 	return nil, nil
 }
 
-func (s *DataService) GetTelemetry(ctx context.Context, req *pb.GetTelemetryRequest) (*pb.GetTelemetryResponse, error) {
+func (s *DataService) GetTelemetry(ctx context.Context, req *services_pb.GetTelemetryRequest) (*services_pb.GetTelemetryResponse, error) {
 	return nil, nil
 }
 
-func (s *DataService) GetFrame(ctx context.Context, req *pb.GetFrameRequest) (*pb.GetFrameResponse, error) {
+func (s *DataService) GetFrame(ctx context.Context, req *services_pb.GetFrameRequest) (*services_pb.GetFrameResponse, error) {
 	return nil, nil
 }
