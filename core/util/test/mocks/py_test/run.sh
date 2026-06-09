3058c236e532401f89c7ef101d44fc93ccfd6be5
Mihir Bala <mkbala42@gmail.com>
2026-06-09 18:07:40 -0400

    Simplified python test run.sh

--- core/util/test/mocks/py_test/run.sh
+++ core/util/test/mocks/py_test/run.sh
@@ -1,8 +1,2 @@
-# Install uv, if we don't already have it
-if command -v uv &> /dev/null; then
-    :
-else
-    curl -LsSf https://astral.sh/uv/install.sh | sh
-fi
 # Run the python script
 uv run main.py
