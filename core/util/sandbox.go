001c3f1e607d9baab7bb73d59426d90f1ee6b641
Aditya Chanana <achanana@cs.cmu.edu>
2026-06-22 11:50:57 -0400

    Switch image to image_ref

--- core/util/sandbox.go
+++ core/util/sandbox.go
@@ -1,10 +1,10 @@
 package util
 
 import (
-    "os"
 	"context"
 	"fmt"
 	"net"
+	"os"
 	"os/exec"
 	"path/filepath"
 
@@ -18,30 +18,30 @@ type SandboxPlugin struct {
 // CreateSandboxPlugin creates a SandboxPlugin backed by bubblewrap (bwrap).
 func CreateSandboxPlugin(options ...PluginOption) (*SandboxPlugin, error) {
 	// Create the plugin
-    internal, err := CreateBasePlugin(options...)
-    if err != nil {
-        return nil, nil
-    }
+	internal, err := CreateBasePlugin(options...)
+	if err != nil {
+		return nil, nil
+	}
 	p := &SandboxPlugin{
 		BasePlugin: internal,
 	}
 	p.runner = "bwrap"
-	
-    // Make sure bubblewrap is installed
+
+	// Make sure bubblewrap is installed
 	_, err = exec.LookPath("bwrap")
 	if err != nil {
 		p.logError(err, "couldn't find bubblewrap (bwrap), have you installed it?")
-        p.cleanup()
+		p.cleanup()
 		return nil, err
 	}
 
-    // Make sure bubblerap has the right permissions
-    err = checkBwrapPermissions()
-    if err != nil {
-        p.logError(err, "bubblewrap couldn't run")
-        p.cleanup()
-        return nil, err
-    }
+	// Make sure bubblerap has the right permissions
+	err = checkBwrapPermissions()
+	if err != nil {
+		p.logError(err, "bubblewrap couldn't run")
+		p.cleanup()
+		return nil, err
+	}
 
 	return p, nil
 }
@@ -49,102 +49,102 @@ func CreateSandboxPlugin(options ...PluginOption) (*SandboxPlugin, error) {
 // Start assembles the bubblewrap arguments, binds in the plugin files, and delegates to BasePlugin.Start.
 func (p *SandboxPlugin) Start(ctx context.Context) (net.Listener, *grpc.ClientConn, error) {
 	// Add the correct bubblewrap args
-    args := []string{
-        "--unshare-all",
-        "--share-net",
+	args := []string{
+		"--unshare-all",
+		"--share-net",
 		"--ro-bind", "/usr", "/usr",
 		"--ro-bind", "/lib", "/lib",
 		"--ro-bind", "/lib64", "/lib64",
 		"--ro-bind", "/bin", "/bin",
-        "--bind", p.runDir, p.runDir,
-        "--ro-bind", "/etc/resolv.conf", "/etc/resolv.conf",
-        "--ro-bind", "/etc/nsswitch.conf", "/etc/nsswitch.conf",
+		"--bind", p.runDir, p.runDir,
+		"--ro-bind", "/etc/resolv.conf", "/etc/resolv.conf",
+		"--ro-bind", "/etc/nsswitch.conf", "/etc/nsswitch.conf",
 		"--proc", "/proc",
 		"--dev", "/dev",
 		"--die-with-parent",
-    }
-    
-    // Link in files
-    for f, w := range p.files {
-        if w == 2 { // executables
-            path, err := exec.LookPath(f)
-            if err != nil {
-                p.logError(err, "couldn't get path to executable")
-                return nil, nil, err
-            }
-            args = append(args,
-                "--ro-bind",
-                path, path, // bind to exact path for executables
-            )
-        } else {
-            _, err := os.Stat(f) // ensure the file exists
-            if err != nil {
-                p.logError(err, "couldn't stat linked file")
-                return nil, nil, err
-            }
-            path, err := filepath.Abs(f)
-            if err != nil {
-                p.logError(err, "couldn't get absolute filepath")
-                return nil, nil, err
-            }
-            if w == 1 { // read-write
-                args = append(args,
-                    "--bind",
-                    path,
-                    fmt.Sprintf("/%s/%s", bindDir, filepath.Base(path)),
-                )
-            } else { // read-only
-                args = append(args,
-                    "--ro-bind",
-                    path,
-                    fmt.Sprintf("/%s/%s", bindDir, filepath.Base(path)),
-                )
-            }
-        }
-    }
-    
-    // Append the existing runner args onto these args,
-    // since we want to preserve any passed in args
-    p.rargs = append(args, p.rargs...)
+	}
 
-    // Only bind in the plugin files if checks are enabled,
-    // otherwise blindly use the script/exec set by the user
-    if p.check {
-        // Bind in the plugin files
-        if p.pkg {
-            p.rargs = append(p.rargs, 
-                "--bind",
-                p.path, bindDir,
-                "--chdir",
-                bindDir,
-            )
-        } else {
-            p.rargs = append(p.rargs, 
-                "--bind",
-                p.script,
-                fmt.Sprintf("/%s/%s", bindDir, filepath.Base(p.script)),
-                "--chdir",
-                bindDir,
-            )
-        }
-        p.script = fmt.Sprintf("./%s", filepath.Base(p.script))
-    }
+	// Link in files
+	for f, w := range p.files {
+		if w == 2 { // executables
+			path, err := exec.LookPath(f)
+			if err != nil {
+				p.logError(err, "couldn't get path to executable")
+				return nil, nil, err
+			}
+			args = append(args,
+				"--ro-bind",
+				path, path, // bind to exact path for executables
+			)
+		} else {
+			_, err := os.Stat(f) // ensure the file exists
+			if err != nil {
+				p.logError(err, "couldn't stat linked file")
+				return nil, nil, err
+			}
+			path, err := filepath.Abs(f)
+			if err != nil {
+				p.logError(err, "couldn't get absolute filepath")
+				return nil, nil, err
+			}
+			if w == 1 { // read-write
+				args = append(args,
+					"--bind",
+					path,
+					fmt.Sprintf("/%s/%s", bindDir, filepath.Base(path)),
+				)
+			} else { // read-only
+				args = append(args,
+					"--ro-bind",
+					path,
+					fmt.Sprintf("/%s/%s", bindDir, filepath.Base(path)),
+				)
+			}
+		}
+	}
+
+	// Append the existing runner args onto these args,
+	// since we want to preserve any passed in args
+	p.rargs = append(args, p.rargs...)
+
+	// Only bind in the plugin files if checks are enabled,
+	// otherwise blindly use the script/exec set by the user
+	if p.check {
+		// Bind in the plugin files
+		if p.pkg {
+			p.rargs = append(p.rargs,
+				"--bind",
+				p.path, bindDir,
+				"--chdir",
+				bindDir,
+			)
+		} else {
+			p.rargs = append(p.rargs,
+				"--bind",
+				p.script,
+				fmt.Sprintf("/%s/%s", bindDir, filepath.Base(p.script)),
+				"--chdir",
+				bindDir,
+			)
+		}
+		p.script = fmt.Sprintf("./%s", filepath.Base(p.script))
+	}
 
 	return p.BasePlugin.Start(ctx)
 }
 
 // checkBwrapPermissions verifies that bwrap can run a minimal sandbox, returning an error if permissions are insufficient.
 func checkBwrapPermissions() error {
-    // Try a minimal bwrap invocation
-    cmd := exec.Command("bwrap",
-        "--ro-bind", "/", "/",
-        "true",
-    )
+	// Try a minimal bwrap invocation
+	cmd := exec.Command("bwrap",
+		"--ro-bind", "/", "/",
+		"true",
+	)
 
-    // If this command doesn't succeed, it might be due to 
-    // a lack of AppArmor permissions
-    if _, err := cmd.CombinedOutput(); err != nil {
-        return fmt.Errorf("bubblewrap has insufficient permissions, could be AppArmor (https://developers.openai.com/codex/concepts/sandboxing)")
-    }
-    return nil
+	// If this command doesn't succeed, it might be due to
+	// a lack of AppArmor permissions
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("bubblewrap has insufficient permissions, could be AppArmor (https://developers.openai.com/codex/concepts/sandboxing)")
+	}
+	return nil
 }
