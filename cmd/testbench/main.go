86e97d719a9e1adc5ce52b81100c6468007fc4f1
Aditya Chanana <achanana@cs.cmu.edu>
2026-07-28 12:52:54 -0400

    update testbench script to use toml config file

--- cmd/testbench/main.go
+++ cmd/testbench/main.go
@@ -14,9 +14,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// PluginConfig models a [vehicles.driver] or [vehicles.mission] table,
-// each of which points at a plugin and a list of CLI-style arguments
-// for that plugin.
+// PluginConfig models a [vehicles.driver] or [vehicles.mission] table, each of
+// which points at a plugin and a list of CLI-style arguments for that plugin.
 type PluginConfig struct {
 	Plugin string   `toml:"plugin"`
 	Args   []string `toml:"args"`
@@ -26,14 +25,14 @@ type PluginConfig struct {
 type Vehicle struct {
 	Name     string        `toml:"name"`
 	Simulate bool          `toml:"simulate,omitempty"`
-	Plugin   string        `toml:"plugin,omitempty"`
 	Driver   *PluginConfig `toml:"driver"`
 	Mission  *PluginConfig `toml:"mission,omitempty"`
 }
 
 // Config models the top-level document: an array of vehicle tables.
 type Config struct {
-	Vehicles []Vehicle `toml:"vehicles"`
+	Vehicles  []Vehicle `toml:"vehicles"`
+	PluginDir string    `toml:"plugin_dir"`
 }
 
 func main() {
@@ -42,17 +41,17 @@ func main() {
 
 	data, err := os.ReadFile(*path)
 	if err != nil {
-		log.Fatalf("reading %s: %v", *path, err)
+		log.Fatal().Msgf("reading %s: %v", *path, err)
 	}
 
 	var cfg Config
 	md, err := toml.Decode(string(data), &cfg)
 	if err != nil {
-		log.Fatalf("parsing TOML: %v", err)
+		log.Fatal().Msgf("parsing TOML: %v", err)
 	}
 
-	// Warn about any keys present in the file that weren't mapped onto
-	// the Config struct
+	// Warn about any keys present in the file that weren't mapped onto the
+	// Config struct
 	if undecoded := md.Undecoded(); len(undecoded) > 0 {
 		fmt.Println("warning: unrecognized keys in config:")
 		for _, k := range undecoded {
@@ -60,59 +59,93 @@ func main() {
 		}
 	}
 
-	var driverPlugin util.Plugin
-	switch *driver {
-	case driverAviary:
-		// Build shim plugin for aviary
-		sockPath := filepath.Join(pluginPath, "aviary", name, "services")
-		driverPlugin, err = util.CreateShimPlugin(sockPath, "")
-		if err != nil {
-			log.Fatal().Err(err).Msg("couldn't create driver plugin")
-		}
-		log.Info().Msg("shim plugin configured")
-	case driverParrotAnafi:
-		if *parrotAnafiPluginPath == "" {
-			log.Fatal().Msgf("parrot-anafi plugin path not set, use -parrot-anafi-plugin-path or %s", parrotAnafiPluginPathEnv)
-		}
-		options := []util.PluginOption{
-			util.WithName(driverParrotAnafi),
-			util.WithPath(*parrotAnafiPluginPath),
-		}
-		if *droneIP != "" {
-			options = append(options, util.WithScriptArgs([]string{"--ip", *droneIP}))
-		}
-		driverPlugin, err = util.CreateBasePlugin(options...)
-		if err != nil {
-			log.Fatal().Err(err).Msg("couldn't create driver plugin")
-		}
-		log.Info().Msg("parrot-anafi plugin configured")
-	default:
-		log.Fatal().Msgf("unknown driver %q, expected %q or %q", *driver, driverAviary, driverParrotAnafi)
+	for _, veh := range cfg.Vehicles {
+		spawnVehicle(&veh, cfg.PluginDir)
+	}
+}
+
+func spawnVehicle(veh *Vehicle, pluginDir string) {
+	if veh.Driver == nil {
+		log.Fatal().Msgf("driver plugin not specified")
+	}
+	driverPlugin := createDriverPlugin(veh, pluginDir)
+	var missionPlugin util.Plugin
+	if veh.Mission != nil {
+		missionPlugin = createMissionPlugin(veh, pluginDir)
 	}
 
 	// Create server listener
+	vehiclePath, err := util.GetVehicleDirByName(veh.Name)
+	if err != nil {
+		log.Fatal().Err(err).Msg("couldn't create vehicle path")
+	}
 	serverLnAddr := filepath.Join(vehiclePath, "server")
 	os.Remove(serverLnAddr)
 	serverLn, err := net.Listen("unix", serverLnAddr)
 	if err != nil {
 		log.Fatal().Err(err).Msg("couldn't listen on server socket")
 	}
-	log.Info().Msg("listening on server.sock")
+	log.Info().Msgf("vehicle %s listening on socket address %s", veh.Name, serverLnAddr)
 
 	// Build plugin config
-	pluginConfig := vehicle.PluginConfig{Driver: driverPlugin}
+	pluginConfig := vehicle.PluginConfig{Driver: driverPlugin, Mission: missionPlugin}
 	videoConfig := vehicle.VideoStreamConfig{StreamType: vehicle.Frames}
 	vehicle, err := vehicle.NewVehicle(
 		pluginConfig,
-		vehicle.WithName(name),
+		vehicle.WithName(veh.Name),
 		vehicle.WithServerListener(serverLn, nil),
 		vehicle.WithVideoStreamConfig(videoConfig),
 	)
 	if err != nil {
 		log.Fatal().Err(err).Msg("couldn't create vehicle")
 	}
-	log.Info().Msg("starting vehicle...")
 	vehicle.Start(context.Background())
-	log.Info().Msg("vehicle started!")
+	log.Info().Msgf("vehicle %s started!", veh.Name)
 	vehicle.Wait()
+}
+
+func createDriverPlugin(veh *Vehicle, pluginDir string) util.Plugin {
+	var driverPlugin util.Plugin
+	if veh.Simulate {
+		// Build shim plugin for aviary
+		sockPath := filepath.Join(pluginDir, "aviary", veh.Name, "services")
+		var err error
+		driverPlugin, err = util.CreateShimPlugin(sockPath, "")
+		if err != nil {
+			log.Fatal().Err(err).Msg("couldn't create aviary plugin")
+		}
+		log.Info().Msg("using aviary driver plugin")
+	} else {
+		pluginPath := filepath.Join(pluginDir, "drivers", veh.Driver.Plugin)
+		options := []util.PluginOption{
+			util.WithName(veh.Name + "-driver"),
+			util.WithPath(pluginPath),
+		}
+		if len(veh.Driver.Args) > 0 {
+			options = append(options, util.WithScriptArgs(veh.Driver.Args))
+		}
+		var err error
+		driverPlugin, err = util.CreateBasePlugin(options...)
+		if err != nil {
+			log.Fatal().Err(err).Msg("couldn't create driver plugin")
+		}
+		log.Info().Msgf("using %s driver plugin", veh.Driver.Plugin)
+	}
+	return driverPlugin
+}
+
+func createMissionPlugin(veh *Vehicle, pluginDir string) util.Plugin {
+	pluginPath := filepath.Join(pluginDir, veh.Mission.Plugin)
+	options := []util.PluginOption{
+		util.WithName(veh.Name + "-mission"),
+		util.WithPath(pluginPath),
+	}
+	if len(veh.Mission.Args) > 0 {
+		options = append(options, util.WithScriptArgs(veh.Mission.Args))
+	}
+	missionPlugin, err := util.CreateBasePlugin(options...)
+	if err != nil {
+		log.Fatal().Err(err).Msg("couldn't create mission plugin")
+	}
+	return missionPlugin
 }
