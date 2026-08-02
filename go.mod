6edffc8984419af46a7b47ce53221a0b6806e47b
Aditya Chanana <achanana@cs.cmu.edu>
2026-08-02 01:03:20 -0400

    Comment out device.go file that doesn't compile

--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	golang.org/x/sync v0.22.0
 	google.golang.org/grpc v1.83.0
 	google.golang.org/protobuf v1.36.11
-	tailscale.com v1.102.0
+	tailscale.com v1.102.1
 )
 
 require (
@@ -26,10 +26,6 @@ require (
 	github.com/agnivade/levenshtein v1.2.1 // indirect
 	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/alexbrainman/sspi v0.0.0-20250919150558-7d374ff0d59e // indirect
-	github.com/aws/aws-sdk-go-v2 v1.42.1 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.32.17 // indirect
-	github.com/aws/smithy-go v1.27.3 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cilium/ebpf v0.17.3 // indirect
 	github.com/coder/websocket v1.8.15 // indirect
@@ -38,10 +34,7 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.2 // indirect
 	github.com/gaissmai/bart v0.29.0 // indirect
-	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-json-experiment/json v0.0.0-20260623181947-01eb4420fa68 // indirect
-	github.com/go-logr/logr v1.4.4 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/goccy/go-json v0.10.6 // indirect
@@ -61,24 +54,18 @@ require (
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc/v3 v3.0.6 // indirect
 	github.com/lestrrat-go/jwx/v3 v3.2.0 // indirect
-	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/lestrrat-go/option/v2 v2.0.0 // indirect
-	github.com/lufia/plan9stats v0.0.0-20260627054121-477a66015f15 // indirect
+	github.com/lufia/plan9stats v0.0.0-20260801144041-2fc331e7910f // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
 	github.com/mattn/go-isatty v0.0.24 // indirect
 	github.com/mdlayher/netlink v1.11.2 // indirect
 	github.com/mdlayher/socket v0.6.1 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
-	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pierrec/lz4/v4 v4.1.26 // indirect
 	github.com/pires/go-proxyproto v0.15.0 // indirect
 	github.com/pkg/sftp v1.13.9 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
-	github.com/prometheus-community/pro-bing v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.24.1 // indirect
-	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.70.1 // indirect
-	github.com/prometheus/procfs v0.21.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20250401214520-65e299d6c5c9 // indirect
 	github.com/safchain/ethtool v0.7.0 // indirect
 	github.com/segmentio/asm v1.2.1 // indirect
@@ -86,7 +73,6 @@ require (
 	github.com/sirupsen/logrus v1.9.4 // indirect
 	github.com/tailscale/certstore v0.1.1-0.20260409135935-3638fb84b77d // indirect
 	github.com/tailscale/go-winio v0.0.0-20231025203758-c4f33415bf55 // indirect
-	github.com/tailscale/goupnp v1.0.1-0.20210804011211-c64d0f06ea05 // indirect
 	github.com/tailscale/hujson v0.0.0-20260727124030-b80ff77dac4f // indirect
 	github.com/tailscale/peercred v0.0.0-20250107143737-35a0c7bd7edc // indirect
 	github.com/tailscale/web-client-prebuilt v0.0.0-20251127225136-f19339b67368 // indirect
@@ -101,11 +87,6 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.2.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
-	go.opentelemetry.io/otel v1.44.0 // indirect
-	go.opentelemetry.io/otel/metric v1.44.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.44.0 // indirect
-	go.opentelemetry.io/otel/trace v1.44.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.4 // indirect
 	go.yaml.in/yaml/v3 v3.0.5 // indirect
 	go4.org/mem v0.0.0-20240501181205-ae6ca9944745 // indirect
@@ -121,7 +102,6 @@ require (
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 	golang.zx2c4.com/wireguard/windows v1.0.1 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260729162451-8efbd57d26e0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gvisor.dev/gvisor v0.0.0-20260731002832-c1b4e12cf268 // indirect
+	gvisor.dev/gvisor v0.0.0-20260802021856-48de7274186a // indirect
 	sigs.k8s.io/yaml v1.6.0 // indirect
 )
--- sdk/device.go
+++ sdk/device.go
@@ -1,124 +1,124 @@
-package sdk
-
-import (
-	"context"
-	"fmt"
-
-	driverpb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/services/driver"
-	vehiclepb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/services/vehicle"
-	commonpb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/common"
-	"github.com/cmusatyalab/steeleagle/sdk/types"
-	"google.golang.org/protobuf/proto"
-)
-
-type Device struct {
-	// TODO: Add cap file here once it is added to util
-	ctx     context.Context
-	control driverpb.ControlServiceClient
-	data    vehiclepb.DataServiceClient
-}
-
-func (d *Device) TakeOff(take_off_altitude float32) *waiter {
-	req := &driverpb.TakeOffRequest{TakeOffAltitude: take_off_altitude}
-	_, err := d.control.TakeOff(d.ctx, req)
-	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
-}
-
-func (d *Device) Land() *waiter {
-	req := &driverpb.LandRequest{}
-	_, err := d.control.Land(d.ctx, req)
-	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped), err)
-}
-
-func (d *Device) Hold() *waiter {
-	req := &driverpb.HoldRequest{}
-	_, err := d.control.Hold(d.ctx, req)
-	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding)
-}
-
-func (d *Device) Kill() *waiter {
-	req := &driverpb.KillRequest{}
-	_, err := d.control.Kill(d.ctx, req)
-	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped)
-}
-
-func (d *Device) SetHome(new_home types.GlobalPosition) *waiter {
-	req := &driverpb.SetHomeRequest{new_home: new_home.AsProto()}
-	_, err := d.control.SetHome(d.ctx, req)
-	return newWaiter(d.ctx, setHomePoller, err)
-}
-
-func (d *Device) ReturnToHome(
-    end_behavior types.ReturnToHomeEndBehavior,
-    min_return_altitude uint32,
-    final_altitude uint32) *waiter {
-    req := &driverpb.ReturnToHomeRequest{
-        end_behavior: end_behavior,
-        min_return_altitude: min_return_altitude,
-        final_altitude: final_altitude,
-    }
-    _, err := d.control.ReturnToHome(d.ctx, req)
-    if end_behavior == types.ReturnToHomeEndBehaviorHover {
-        return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
-    } else {
-        return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped), err)
-    }
-}
-
-func (d *Device) GoToGlobalPosition(
-    position types.GlobalPosition,
-    heading_mode types.HeadingMode,
-    altitude_mode types.AltitudeMode,
-    speed float32,
-    angular_speed float32) {
-    req := &driverpb.GoToGlobalPositionRequest{
-        position: commonpb.GlobalPosition{
-            latitude: position.latitude,
-            longitude: position.longitude,
-            altitude: position.altitude,
-            heading: position.heading,
-        },
-        heading_mode: heading_mode,
-        altitude_mode: altitude_mode,
-        speed: speed,
-        angular_speed: angular_speed,
-    }
-    _, err := d.control.GoToGlobalPosition(d.ctx, req)
-    return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
-}
-
-func (d *Device) GoToRelativePosition(
-    position types.RelativePosition,
-    speed float32,
-    angular_speed float32,
-    frame types.ReferenceFrame) {
-    req := &driverpb.GoToRelativePositionRequest{
-        position: commonpb.RelativePosition{
-            x: position.x,
-            y: position.y,
-            z: position.z,
-            angle: position.angle,
-        },
-        speed: speed,
-        angular_speed: angular_speed,
-        frame: frame,
-    }
-    _, err := d.control.GoToRelativePosition(d.ctx, req)
-    return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
-}
-
-func (d *Device) SetVelocity(velocity types.Velocity, frame types.ReferenceFrame) {
-    req := &driverpb.SetVelocityRequest{
-        velocity: commonpb.Velocity{
-            x_vel: velocity.x_vel,
-            y_vel: velocity.y_vel,
-            z_vel: velocity.z_vel,
-            angular_vel: velocity.angular_vel,
-        },
-        frame: frame,
-    }
-    _, err := d.control.SetVelocity(d.ctx, req)
-    return newWaiter(d.ctx, velocityPoller(req, d.data), err)
-}
-
-
+//package sdk
+//
+//import (
+//	"context"
+//	"fmt"
+//
+//	driverpb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/services/driver"
+//	vehiclepb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/services/vehicle"
+//	commonpb "github.com/cmusatyalab/steeleagle/api/go/steeleagle_protocol/v1/common"
+//	"github.com/cmusatyalab/steeleagle/sdk/types"
+//	"google.golang.org/protobuf/proto"
+//)
+//
+//type Device struct {
+//	// TODO: Add cap file here once it is added to util
+//	ctx     context.Context
+//	control driverpb.ControlServiceClient
+//	data    vehiclepb.DataServiceClient
+//}
+//
+//func (d *Device) TakeOff(take_off_altitude float32) *waiter {
+//	req := &driverpb.TakeOffRequest{TakeOffAltitude: take_off_altitude}
+//	_, err := d.control.TakeOff(d.ctx, req)
+//	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
+//}
+//
+//func (d *Device) Land() *waiter {
+//	req := &driverpb.LandRequest{}
+//	_, err := d.control.Land(d.ctx, req)
+//	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped), err)
+//}
+//
+//func (d *Device) Hold() *waiter {
+//	req := &driverpb.HoldRequest{}
+//	_, err := d.control.Hold(d.ctx, req)
+//	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding)
+//}
+//
+//func (d *Device) Kill() *waiter {
+//	req := &driverpb.KillRequest{}
+//	_, err := d.control.Kill(d.ctx, req)
+//	return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped)
+//}
+//
+//func (d *Device) SetHome(new_home types.GlobalPosition) *waiter {
+//	req := &driverpb.SetHomeRequest{new_home: new_home.AsProto()}
+//	_, err := d.control.SetHome(d.ctx, req)
+//	return newWaiter(d.ctx, setHomePoller, err)
+//}
+//
+//func (d *Device) ReturnToHome(
+//    end_behavior types.ReturnToHomeEndBehavior,
+//    min_return_altitude uint32,
+//    final_altitude uint32) *waiter {
+//    req := &driverpb.ReturnToHomeRequest{
+//        end_behavior: end_behavior,
+//        min_return_altitude: min_return_altitude,
+//        final_altitude: final_altitude,
+//    }
+//    _, err := d.control.ReturnToHome(d.ctx, req)
+//    if end_behavior == types.ReturnToHomeEndBehaviorHover {
+//        return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
+//    } else {
+//        return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusStopped), err)
+//    }
+//}
+//
+//func (d *Device) GoToGlobalPosition(
+//    position types.GlobalPosition,
+//    heading_mode types.HeadingMode,
+//    altitude_mode types.AltitudeMode,
+//    speed float32,
+//    angular_speed float32) {
+//    req := &driverpb.GoToGlobalPositionRequest{
+//        position: commonpb.GlobalPosition{
+//            latitude: position.latitude,
+//            longitude: position.longitude,
+//            altitude: position.altitude,
+//            heading: position.heading,
+//        },
+//        heading_mode: heading_mode,
+//        altitude_mode: altitude_mode,
+//        speed: speed,
+//        angular_speed: angular_speed,
+//    }
+//    _, err := d.control.GoToGlobalPosition(d.ctx, req)
+//    return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
+//}
+//
+//func (d *Device) GoToRelativePosition(
+//    position types.RelativePosition,
+//    speed float32,
+//    angular_speed float32,
+//    frame types.ReferenceFrame) {
+//    req := &driverpb.GoToRelativePositionRequest{
+//        position: commonpb.RelativePosition{
+//            x: position.x,
+//            y: position.y,
+//            z: position.z,
+//            angle: position.angle,
+//        },
+//        speed: speed,
+//        angular_speed: angular_speed,
+//        frame: frame,
+//    }
+//    _, err := d.control.GoToRelativePosition(d.ctx, req)
+//    return newWaiter(d.ctx, basePoller(req, d.data, types.MotionStatusHolding), err)
+//}
+//
+//func (d *Device) SetVelocity(velocity types.Velocity, frame types.ReferenceFrame) {
+//    req := &driverpb.SetVelocityRequest{
+//        velocity: commonpb.Velocity{
+//            x_vel: velocity.x_vel,
+//            y_vel: velocity.y_vel,
+//            z_vel: velocity.z_vel,
+//            angular_vel: velocity.angular_vel,
+//        },
+//        frame: frame,
+//    }
+//    _, err := d.control.SetVelocity(d.ctx, req)
+//    return newWaiter(d.ctx, velocityPoller(req, d.data), err)
+//}
+//
+//
