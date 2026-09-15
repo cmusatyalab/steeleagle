08988c6f9e79cb2e1a373ac6c4d21743e1d4c799
Aditya Chanana <achanana@cs.cmu.edu>
2026-09-15 13:56:20 -0400

    Relax tolerances for mission sdk velocities

--- sdk/waiter.go
+++ sdk/waiter.go
@@ -43,10 +43,10 @@ func (w *waiter[Resp]) Wait(options ...opt.WaitOption) (Resp, error) {
 		Interval: 100 * time.Millisecond,
 		Stall:    5 * time.Second,
 		Tolerances: opt.Tolerances{
-			PosTol:      1.5, // meters
-			AngleTol:    2.0, // degrees
+			PosTol:      3.0, // meters
+			AngleTol:    5.0, // degrees
 			SpeedTol:    0.5, // meters/second
-			AngSpeedTol: 0.5, // degrees/second
+			AngSpeedTol: 5.0, // degrees/second
 		},
 	}
 	for _, option := range options {
